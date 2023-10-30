b1efac90b5d3aeacc73eb3f82d08bbd1fe75e589
Kunal <87492428+boaluma@users.noreply.github.com>
2023-10-30 08:56:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI 
 from langchain.chains.question_answering import load_qa_chain
-
+import openai
  
 # Sidebar contents
 with st.sidebar:
@@ -30,6 +30,29 @@ with st.sidebar:
 os.environ["OPENAI_API_KEY"] = st.secrets["key"] 
 load_dotenv()
 
+previous_conversation = []
+
+def ask_question(question, context):
+    global previous_conversation
+    input_text = ''
+    if previous_conversation:
+        input_text = '\n'.join(f'Q: {q}\nA: {a}' for q, a in previous_conversation)
+        input_text += f'\nQ: {question}\n'
+    else:
+        input_text = f'Q: {question}\n'
+
+    input_text += context
+
+    response = openai.Completion.create(
+        engine="davinci", 
+        prompt=input_text, 
+        max_tokens=100
+    )
+
+    answer = response.choices[0].text.strip()
+    previous_conversation.append((question, answer))
+    return answer
+
  
 def main():
     st.header("PDF Q&A app")
@@ -47,37 +70,40 @@ def main():
         for page in pdf_reader.pages:
             text += page.extract_text()
  
-        text_splitter = RecursiveCharacterTextSplitter(
-            chunk_size=1000,
-            chunk_overlap=200,
-            length_function=len
-            )
-        chunks = text_splitter.split_text(text=text)
+        # text_splitter = RecursiveCharacterTextSplitter(
+        #     chunk_size=1000,
+        #     chunk_overlap=200,
+        #     length_function=len
+        #     )
+        # chunks = text_splitter.split_text(text=text)
  
-        # # embeddings
-        store_name = pdf.name[:-4]
-        st.write(f'{store_name}')
+        # # # embeddings
+        # store_name = pdf.name[:-4]
+        # st.write(f'{store_name}')
  
-        if os.path.exists(f"{store_name}.pkl"):
-            with open(f"{store_name}.pkl", "rb") as f:
-                VectorStore = pickle.load(f)
+        # if os.path.exists(f"{store_name}.pkl"):
+        #     with open(f"{store_name}.pkl", "rb") as f:
+        #         VectorStore = pickle.load(f)
             
-        else:
-            embeddings = OpenAIEmbeddings()
-            VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
-            with open(f"{store_name}.pkl", "wb") as f:
-                pickle.dump(VectorStore, f)
+        # else:
+        #     embeddings = OpenAIEmbeddings()
+        #     VectorStore = FAISS.from_texts(chunks, embedding=embeddings)
+        #     with open(f"{store_name}.pkl", "wb") as f:
+        #         pickle.dump(VectorStore, f)
  
         # Input user query
         query = st.text_input("Enter the question regarding your PDF :")
  
         if query:
-            docs = VectorStore.similarity_search(query=query, k=5)
- 
-            llm = OpenAI()
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            response = chain.run(input_documents=docs, question=query)
+            response = ask_question(query, text)
             st.write(response)
+            # docs = VectorStore.similarity_search(query=query, k=5)
+            # llm = OpenAI()
+            # chain = load_qa_chain(llm=llm, chain_type="stuff")
+            # response = chain.run(input_documents=docs, question=query)
+            # st.write(response)
+            
+         
  
 if __name__ == '__main__':
     main()
